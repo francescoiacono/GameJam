9b7b878419327380485a4e75d68856bdef6d971f
Francesco <sennozeta@gmail.com>
2018-09-27 18:44:04 +0100

    Developing UI

---
+++ GameJam/Assets/MoneyManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyManager : MonoBehaviour {
+
+	public Text moneyText;
+    public double salary;
+    public Button workBtn;
+    public Image timebar;
+    public float workTime;
+    
+    private bool working;
+    private bool isGameStarted;
+   
+    protected double moneyOwned;
+	
+    // Use this for initialization
+	void Start ()
+    {
+        working = false;
+        moneyOwned = 0;
+        workBtn.onClick.AddListener(ActivatingWork);
+        StartCoroutine(WaitAndEarn(workTime));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		isGameStarted = gameObject.GetComponent<StartGame>().gameStarted;
+		DisplayMoney ();
+    }
+
+	void DisplayMoney() {
+		if (isGameStarted == true) {
+			moneyText.text = "$" + moneyOwned; 
+		}
+	}
+
+    void ActivatingWork() {
+        working = !working;
+    }
+    
+    private IEnumerator WaitAndEarn(float wait) {
+        while (true) {
+            yield return new WaitForSeconds(wait);
+
+            if (working) {                                 
+                moneyOwned += salary;
+            }
+
+        }
+    }
+
+}
---
+++ GameJam/Assets/StartGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour {
+
+	// Shows the main buttons after the user is ready to start
+	public Button startBtn;
+	public GameObject workPanel;
+	public GameObject statsPanel;
+	[HideInInspector]public bool gameStarted;
+
+	// Use this for initialization
+	void Start () {
+		gameStarted = false;
+		workPanel.gameObject.SetActive (false);
+		statsPanel.gameObject.SetActive (false);
+		startBtn.onClick.AddListener (ShowUI);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ShowUI() {
+		gameStarted = true;
+		startBtn.gameObject.SetActive (false);
+		workPanel.gameObject.SetActive (true);
+		statsPanel.gameObject.SetActive (true);
+	}
+}
